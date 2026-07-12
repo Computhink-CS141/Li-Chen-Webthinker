4725267c28bc7dc8940ba0d06c6c930c004edb13
Computhink-CS141 <cs141@computhink.com.sg>
2026-07-12 04:08:33 +0000

    Jul 12, 2026, 4:08 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -12,7 +12,7 @@ function draw() {
   // Challenge 1: An eye using ellipse() function
   fill("gold");
     //    x   y  diameter
-  ellipse(80, 80, 100);
+  ellipse(80, 60, 100);
 
   // Challenge 2: A face using circle() function
 
