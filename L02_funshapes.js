18e4f653f75af99931b435bc0beb55fae5e6dd07
Computhink-CS141 <cs141@computhink.com.sg>
2026-07-12 04:20:25 +0000

    Jul 12, 2026, 4:20 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -21,7 +21,7 @@ function draw() {
   // Challenge 3: A house using rect() function
   rect(100, 200, 100, 200)
   // Challenge 4: A rocketship using triangle() function
-  //       
+  //       x1  x2  
   triangle(30, 75, 58, 20, 86, 75)
   // Challenge 5: A square face using quad() function
 
