053068cdecd745654e282e1d4640b9f8a51146b1
Computhink-CS141 <cs141@computhink.com.sg>
2026-07-19 04:15:13 +0000

    Jul 19, 2026, 4:15 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -22,7 +22,7 @@ let circleY = 50;
 // circle(375, 200, 100)
 // circle(525, 200, 100)
   // Task 1: Colour Gradient
-  for (let i = 0; i < 5; 1++) {
+  for (let i = 0; i < 5; i++) {
     fill(i * 50)
     circle(50 + i * 50, circleY, circleDiameter)
   }
