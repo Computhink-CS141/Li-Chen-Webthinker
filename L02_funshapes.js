c11c23c2d6e7a7250452327d4784d28cf2e21d40
Computhink-CS141 <cs141@computhink.com.sg>
2026-07-19 03:29:53 +0000

    Jul 19, 2026, 3:29 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -25,7 +25,7 @@ function draw() {
   ellipse(200, 300, 200, 400)
   // x1 is a smaller no. than x2
   //       x1  y1     x2  y2   x3  y3
-  triangle(170, 180, 206, 70, 252, 180)
+  triangle(170, 180, 206, 70, 272, 180)
   rect(100, 450, 200, 50)
   circle(200, 300, 50)
   // Challenge 5: A square face using quad() function
