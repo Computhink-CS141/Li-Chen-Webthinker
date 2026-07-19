6d8f813f7717540f8fc4d3487d71140125a186d7
Computhink-CS141 <cs141@computhink.com.sg>
2026-07-19 04:03:54 +0000

    Jul 19, 2026, 4:03 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -10,7 +10,7 @@ let circleDiameter = 50;
 let circleY = 50;
 
 for (let num=1; num<9; num++){
-  circle(50*num,circleY+=6, circleDiameter);
+  circle(50*num,circleY+=10, circleDiameter);
 }
 
 // circle(225, 200, 100)
