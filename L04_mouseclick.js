e12e539fbc36c7acb53f5523fa4dd426ac11db98
Computhink-CS141 <cs141@computhink.com.sg>
2026-07-26 03:43:30 +0000

    Jul 26, 2026, 3:43 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -3,18 +3,19 @@ function setup() {
     //            w    h
     createCanvas(500, 500);
     background(220);
-    // noLoop();
+    noLoop();
 }
 
-function draw() {
-    let ypos = 50;
-    let xpos = 50;
-    let ggColour = 0;
-    for (let num = 0; num < 6; num++){
-        fill(0, ggColour, 0);
-        circle(xpos, ypos, 50);
-        xpos = xpos+50;
-        ypos = ypos+50;
-        ggColour = ggColour+50;
-    }
-}
+// function draw() {
+//     let ypos = 50;
+//     let xpos = 50;
+//     let ggColour = 0;
+//     for (let num = 0; num < 6; num++){
+//         fill(0, ggColour, 0);
+//         circle(xpos, ypos, 50);
+//         xpos = xpos+50;
+//         ypos = ypos+50;
+//         ggColour = ggColour+50;
+//     }
+// }
+
