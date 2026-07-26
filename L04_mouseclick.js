582e053493783e2b31555eb8f38d1fb921da36c0
Computhink-CS141 <cs141@computhink.com.sg>
2026-07-26 03:29:19 +0000

    Jul 26, 2026, 3:29 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -9,6 +9,7 @@ function setup() {
 function draw() {
     let ypos = 50;
     let xpos = 50;
+    let ggColour
     for (let num = 0; num < 6; num++){
         fill(220);
         circle(xpos, 50, 50);
