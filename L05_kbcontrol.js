0fc2d4b0845b4159d059743b3ed6a6ba0ccd796a
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-02 03:45:04 +0000

    Aug 2, 2026, 3:45 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -39,7 +39,7 @@ function setup() {
 function draw() {
     background("skyblue");
     if (showCircle) {
-        circle(width/2, height/2, size);
+        rec(width/2, height/2, size);
     }
 }
 
