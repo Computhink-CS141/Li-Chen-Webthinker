7db2036c8232831c78e381696d54aa7e43a56f9e
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-02 03:54:47 +0000

    Aug 2, 2026, 3:54 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -59,10 +59,10 @@ function keyPressed() {
         // ! is a NOT operator
         showCircle = ! showCircle;
     }
-    if (key === s) (
-        showSquare = ! show
-    )
-    if (key === r) (
-        showRect = ! showRect
-    )
+    if (key === s) {
+        showSquare = ! showSquare;
+    }
+    if (key === r) {
+        showRect = ! showRect;
+    }
 }
