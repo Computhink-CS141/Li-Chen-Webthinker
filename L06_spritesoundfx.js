a7e9e64a1f45ca63fcdc1c7d6f557ca844c8dc89
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-16 04:17:05 +0000

    Aug 16, 2026, 4:17 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -20,7 +20,7 @@ function setup() {
 
 function draw() {
     background("lightblue")
-    //          x  y   w    h
+    //          x     y   w    h
     image(pico, xpos, 0, 110, 133);
     if (keyIsDown(RIGHT_ARROW)) {
         xpos = xpos + 2;
