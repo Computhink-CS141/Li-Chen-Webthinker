90024b15bc29a04eee650f200ef811707dd4d676
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-16 04:09:20 +0000

    Aug 16, 2026, 4:09 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -7,7 +7,7 @@ let bgm, sfx;
 function preload() {
     // any media, image or sound must load here
     pico = loadImage("assets/pico-a.png")
-    bgm = loadSound("assets/")
+    bgm = loadSound("assets/bossaNova.mp3")
 }
 
 function setup() {
