71935ab0a88807b47cad49734a47e9fd85a2a952
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-23 04:05:40 +0000

    Aug 23, 2026, 4:05 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -23,7 +23,7 @@ function draw() {
     ballY = ballY + ballSpeedY;
 
     if (ballX > width) {
-        ballSpeedX = -1 * ballSpeedX; // 2 -1 = -2
+        ballSpeedX = -1.2 * ballSpeedX; // 2 -1 = -2
         fill(random(0, 255), 0, random(0, 255), random(10, 55));
     }
     if (ballX < 0) {
