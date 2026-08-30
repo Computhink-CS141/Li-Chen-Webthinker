152d7d6d1d85cccaf57f773f0d2de76fffc87704
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-30 03:40:29 +0000

    Aug 30, 2026, 3:40 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -19,7 +19,7 @@ function draw() {
 
     for (let count=0; count < storyText.length ; count++) {
         //
-        text(storyText[count], 100, ypos + lineY)
+        text(storyText[count], 100, ypos + lineY);
     }
     // //                       x    y
     // text("hello everybody", 100, ypos);
