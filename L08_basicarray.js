f03c1149cb51ba8e8a26f5ad830a3f6880df2400
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-30 03:19:33 +0000

    Aug 30, 2026, 3:19 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -8,6 +8,6 @@ function draw() {
     background(220); //erase canvas
     textSize(64);
     //                       x      y
-    text("hello everybody", 150, height/2);
-    text("second line",     1500, height/2 + 18);
+    text("hello everybody", 17, height/2);
+    text("second line",     150, height/2 + 18);
 }
