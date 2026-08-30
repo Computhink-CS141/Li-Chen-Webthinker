01b541c79e59a8ca3fe748ef628d53b3057afab3
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-30 03:34:48 +0000

    Aug 30, 2026, 3:34 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -10,7 +10,7 @@ function setup() {
     storyText[0] = "long long long long long long long long time ago.................";
     storyText[1] = "i was told a grandmother story";
     storyText[2] = "the end."
-    print
+    print(storyText)
 }
 
 function draw() {
