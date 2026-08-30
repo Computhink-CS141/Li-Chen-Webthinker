a869d8a7156e8ffdeab65107c2b767aef2775076
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-30 03:20:23 +0000

    Aug 30, 2026, 3:20 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -2,13 +2,13 @@
 let ypos = 0;
 
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(400, 600);
     
 }
 
 function draw() {
     background(220); //erase canvas
-    textSize(64);
+    textSize(18);
     //                       x      y
     text("hello everybody", 175, height/2);
     text("second line",     175, height/2 + 18);
