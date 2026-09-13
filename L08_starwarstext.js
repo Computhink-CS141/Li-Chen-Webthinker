de94186372a8398681fe7bddbcc2f24b4231108e
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-13 04:23:20 +0000

    Sep 13, 2026, 4:23 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -38,12 +38,6 @@ function draw() {
         fill(255,255,0)
     textSize(20);
     textAlign(CENTER);
-    for(let i = 0; i<storyText.length; i++) {
-        text((i+1) + "." + storyText[i], width/2, yPos + i*lineGap);
-    }
-    yPos -= 0.6;
-    if(yPos < -storyText.length * lineGap) {
-        yPos = height;
-    }
+    text("")
     }
 }
