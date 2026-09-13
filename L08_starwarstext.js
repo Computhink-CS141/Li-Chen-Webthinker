ac56fc5162bdf6e053ff3583ab03cdfa5b3bb546
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-13 03:12:37 +0000

    Sep 13, 2026, 3:12 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,1 +1,14 @@
 // write your codes here
+let name = "lc";
+
+function setup () {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(220);
+    textSize(20);
+    text("li chen", 50 ,50);
+    textSize(67)
+    text(67);
+}
