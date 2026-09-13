db12926079ba2cf4eb5b0be49df018d8df4e209b
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-13 03:49:34 +0000

    Sep 13, 2026, 3:49 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,6 +1,6 @@
 // write your codes here
 let storyText = [
-    "A long time agoo in a galaxy far",
+    "A long time ago in a galaxy far",
     "far away",
     "It is a period of learning",
     "Our adventure begins here",
