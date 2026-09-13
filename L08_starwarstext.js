ef2a0e5839ac078d958eb57b2cc22052d59689ba
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-13 03:23:58 +0000

    Sep 13, 2026, 3:23 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -20,6 +20,7 @@ function draw() {
     textAlign(LEFT);
     text["My favourite foods:"]
     for(let i=0; i<list.length;i++) {
-        text((i+1) + "." + list[i], 50, 1*30)
+        text((i+1) + "." + list[i], 50, 1*30);
+        
     }
 }
