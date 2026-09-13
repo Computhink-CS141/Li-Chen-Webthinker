8ebf697aa3f3db1e4490b6c2ffc5fd42c3dbed16
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-13 03:13:29 +0000

    Sep 13, 2026, 3:13 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -9,7 +9,8 @@ function draw() {
     background(220);
     textSize(20);
     text("li chen", 50 ,50);
-    textSize(15)
+    textSize(15);
     text("12", 50, 60);
-    textSize()
+    textSize(10);
+    text
 }
