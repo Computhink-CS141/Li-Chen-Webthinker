31f7cae0bafd494e911083513a929b705163e651
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-20 04:10:26 +0000

    Sep 20, 2026, 4:10 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -41,6 +41,7 @@ function updateCountdown() {
     if (countdown === 0) {
         clearInterval(objectId);
         bgColor = "red";
+        sfx.play
     }
     else {
         countdown = countdown - 1;
