90a56852f16a0e2544eeaa43df36ff964737f655
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-20 03:42:07 +0000

    Sep 20, 2026, 3:42 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -17,7 +17,6 @@ function draw() {
     fill(0);
     textSize(72);
     text(nf(hh,2)+ ":"+(nf(min, 2)) + ":"+(nf(sec,2)), width/2, height/2);
-
-
-    displayText(countdown)
+    
+    text(countdown)
 }
